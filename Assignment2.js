dc48ee070001458ebd380cf9b5493d772b027a62
Eduardo Martinez <eduardo.martinez@integritas.mx>
2016-04-19 10:06:53 -0500

    Corrigiendo las barbaridades de JC

--- Assignment2.js
+++ Assignment2.js
@@ -29,10 +29,10 @@ function interpretExpr(e, state) {
     if (e.type == NUM) { return e.val; }
     if (e.type == FALSE) { return false; }
     if (e.type == VAR) { return interpretExpr(e.name, state);}
-    if (e.type == TIMES) { return interpretExpr(e.left, state) + interpretExpr(e.right, state) }
-    if (e.type == LT) { return interpretExpr(e.left, state) + interpretExpr(e.right, state) }
-    if (e.type == AND) { return interpretExpr(e.left, state) + interpretExpr(e.right, state) }
-    if (e.type == NOT) { return interpretExpr(e.left, state)}
+    if (e.type == TIMES) { return interpretExpr(e.left, state) * interpretExpr(e.right, state) }
+    if (e.type == LT) { return interpretExpr(e.left, state) < interpretExpr(e.right, state) }
+    if (e.type == AND) { return interpretExpr(e.left, state) && interpretExpr(e.right, state) }
+    if (e.type == NOT) { return !interpretExpr(e.left, state)}
     if (e.type == PLUS) {  return interpretExpr(e.left, state) + interpretExpr(e.right, state) }
 
 }
